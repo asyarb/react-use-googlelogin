b990906f758882a5bb41e8f21077e392ee092c0b
git <asyarb@hawaii.edu>
2020-05-16 02:12:45 -1000

    docs: remove console log

--- examples/refreshing-tokens/src/index.tsx
+++ examples/refreshing-tokens/src/index.tsx
@@ -14,7 +14,6 @@ const App = () => {
   } = useGoogleAuth()
 
   const handleRequest = async () => {
-    console.log(googleUser.expiresAt, googleUser.accessToken)
     await fetchWithRefresh('/')
   }
 
